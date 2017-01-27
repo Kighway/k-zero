ac4addafc435454f46f8e518d887d69f117707b7
github username <github email address>
2017-01-27 15:31:40 -0500

    fixed syntax

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,14 +1,14 @@
 var count = 0;
 
-var counter = setInterval(timer, 1000)
+// var counter = setInterval(timer, 1000)
 
 function timer() {
   count++
-  if ( gameOver )
-  {
-     clearInterval(counter)
-     return
-  }
+  // if ( gameOver )
+  // {
+  //    clearInterval(counter)
+  //    return
+  // }
   //show the number of seconds here
   document.getElementById("timer").innerHTML = count
 }
