52e47fef228c1aae1ec3b76ffc6f6f9323687a7c
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-01-28 15:48:33 -0500

    wormhole logic siloed to wormhole js

--- javascripts/wormhole.js
+++ javascripts/wormhole.js
@@ -1,20 +1,46 @@
-function wormholePulse(leftPoint) {
-  const wormhole = document.createElement('div')
-  wormhole.className = 'wormhole'
-  wormhole.style.left = `${leftPoint}px`
-  var top = wormhole.style.top = 0
-  VIEW.appendChild(wormhole)
-  function wormHoleFluctuation() {
-    wormhole.style.top = `${top += 6}px`;
-    if (breachWormhole(wormhole)) {
-      gameOver = true
-      endGame()
+
+var i = 0
+breachInterval = 0
+currentBreach = 0
+lastBreach = 0
+gameOver = false
+
+function graphOneSine () {
+    wormholePulse(SINE[i++])
+    if (i  === SINE.length - 1) {
+      i = 0
     }
-    if (top < YAXIS) {
-      window.requestAnimationFrame(wormHoleFluctuation)
-    } else {
-      wormhole.remove()
+}
+
+function breachWormhole(wormhole) {
+  const top = positionToInteger(wormhole.style.top)
+  if (top > 620) {
+    const leftWing = positionToInteger(SHIP.style.left)
+    const rightWing = leftWing + 68;
+    const wormholeLeftEdge = positionToInteger(wormhole.style.left)
+    const wormholeRightEdge = wormholeLeftEdge + 200;
+
+    if (lastBreach === 0) {
+      lastBreach = (new Date()).getTime()
     }
+
+    if (
+      (wormholeLeftEdge <= leftWing && wormholeRightEdge >= leftWing) ||
+      (wormholeLeftEdge >= leftWing && wormholeRightEdge <= rightWing) ||
+      (wormholeLeftEdge <= rightWing && wormholeRightEdge >= rightWing)
+    ) {
+         currentBreach = (new Date()).getTime()
+         breachInterval = currentBreach - lastBreach
+
+        if (breachInterval > 34)
+        {
+          return true
+        }
+      lastBreach = currentBreach
+      }
   }
-  window.requestAnimationFrame(wormHoleFluctuation)
+}
+
+function positionToInteger(p) {
+  return parseInt(p.split('px')[0]) || 0
 }
