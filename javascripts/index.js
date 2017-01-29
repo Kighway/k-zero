3d61bb3b148b5720be9935e8f2d3be2c4719bd88
github username <github email address>
2017-01-29 13:33:19 -0500

    added a lot more audio - designed sequence of audio tracks

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,10 +3,13 @@ function start() {
   $("#starwars").remove()
   moveShip()
   graphSine = setInterval(graphOneSine, 44)
-  $("audio")[0].play()
   counter = setInterval(timer, 1000)
   setTimeout( () => {$("#timer").css("font-size", "100px")}, 1000)
-
+  $("audio")[0].pause()
+  $("audio")[1].play()
+  setTimeout(function(){ $("audio")[2].play() }, 4200)
+  setTimeout(function(){ $("audio")[3].play() }, 21000)
+  setTimeout(function(){ $("audio")[4].play() }, 37000)
 }
 
 function endGame() {
@@ -17,6 +20,7 @@ function endGame() {
   $("#view").append(explosion)
   setTimeout( () => { $("#view").children().remove() } , 250)
   setTimeout(() => { $("#view").css('background-image', 'url(./images/vader.jpg)') }, 200)
-  $("audio")[1].play()
-  $("audio")[2].play()
+  $("audio")[5].play()
+  $("audio")[6].play()
+  $("audio")[7].play()
 }
