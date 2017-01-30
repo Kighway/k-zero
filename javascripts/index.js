b9cfaba6c806908ae28fbcdf2f98309b278cc0fe
github username <github email address>
2017-01-30 11:30:09 -0500

    now have ability to choose ship

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+  $('.choose-millennium-falcon').click(function(){
+    $("#ship").attr("src", "./images/millennium-falcon.jpg")
+    $("#ship").css("opacity", "1.0")
+  })
+
+  $('.choose-x-wing').click(function(){
+    $("#ship").attr("src", "./images/x-wing.jpg")
+    $("#ship").css("opacity", "1.0")
+  })
+})
+
 function start() {
   document.addEventListener('keydown', moveShip)
   $("#starwars").remove()
@@ -14,7 +26,7 @@ function start() {
 function endGame() {
   clearInterval(counter)
   clearInterval(graphSine)
-  var explosion = `<img id="explosion" src="images/explosion.gif" style="bottom: 30px; left: ${$("#ship").css("left")};">`
+  var explosion = `<img id="explosion" src="images/explosion.gif" style="bottom: 70px; left: ${$("#ship").css("left")};">`
   $("#ship").remove()
   $("#view").append(explosion)
   setTimeout( () => { $("#view").children().remove() } , 250)
