366dc782a92ee903491a8ee33ec4310848f44aec
github username <github email address>
2017-01-30 14:34:49 -0500

    added explosion sound and unique ship crashes audio

--- javascripts/index.js
+++ javascripts/index.js
@@ -17,21 +17,28 @@ function start() {
   graphSine = setInterval(graphOneSine, 40)
   counter = setInterval(timer, 1000)
   setTimeout( () => {$("#timer").css("font-size", "100px")}, 1000)
-  $("audio")[0].pause()
-  $("audio")[1].play()
-  setTimeout(function(){ $("audio")[2].play() }, 4200)
-  setTimeout(function(){ $("audio")[3].play() }, 20000)
+  $("audio")[0].pause() //star wars intro music ends
+  $("audio")[1].play() //admiral ackbar hyperspace
+  setTimeout(function(){ $("audio")[2].play() }, 4200) //full rogue squadron voice over
+  setTimeout(function(){ $("audio")[3].play() }, 20000) //endor music
 }
 
 function endGame() {
   clearInterval(counter)
   clearInterval(graphSine)
+  if ( $("#ship").attr("src").includes("x-wing") ) {
+    $("audio")[4].play() //r2-d2 screaming
+  }
+  else {  //if ( $("#ship").attr("src").includes("millennium-falcon") )
+    $("audio")[5].play() //chewie screaming
+  }
   var explosion = `<img id="explosion" src="images/explosion.gif" style="bottom: 70px; left: ${$("#ship").css("left")};">`
   $("#ship").remove()
   $("#view").append(explosion)
   setTimeout( () => { $("#view").children().remove() } , 250)
   setTimeout(() => { $("#view").css('background-image', 'url(./images/vader.jpg)') }, 200)
-  $("audio")[4].play()
-  $("audio")[5].play()
-  $("audio")[6].play()
+  $("audio")[6].play() //explosion
+  $("audio")[7].play() //imperial march
+  $("audio")[8].play() //dark side
+  $("audio")[9].play() //vader breathing
 }
